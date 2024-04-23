92a78b30f171aa9ca46c4d0bba141c71bf20c36e
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-04-23 21:31:23 +0200

    edited

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -10,6 +10,8 @@ class MoveableObject {
     otherDirection = false;
     speedY = 0;
     acceleration = 2.5;
+    energy = 100;
+    lastHit = 0;
 
     applyGavity() {
         setInterval(() => {
@@ -59,6 +61,25 @@ class MoveableObject {
 
     }
 
+    hit() {
+        this.energy -= 5;
+        if(this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1
+    }
+
+    isDead() {
+        return this.energy == 0
+    }
+
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
@@ -68,7 +89,7 @@ class MoveableObject {
     }
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
--- models/world.class.js
+++ models/world.class.js
@@ -23,7 +23,7 @@ class World {
         setInterval(() => {
             this.level.enemies.forEach((enemy) => {
                 if(this.character.isColliding(enemy)) {
-                    console.log('Kollision');
+                    this.character.hit();
                 }
             })
         }, 200);
